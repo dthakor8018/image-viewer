5153d8acef33c7f6d685750507efee63f6c858bf
dthakor <dthakor@vmware.com>
2020-02-04 17:36:53 +0530

    like dislike for card

--- src/screens/home/ImageCard.js
+++ src/screens/home/ImageCard.js
@@ -10,6 +10,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import {red} from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import Grid from "@material-ui/core/Grid";
 import "./ImageCard.css";
 import TextField from "@material-ui/core/TextField";
@@ -46,11 +47,19 @@ function changeToDate(created_time) {
 export default function ImageCard(props) {
   const classes = useStyles();
     const [comment, setComment] = React.useState("");
+    const [render, setRender] = React.useState(false);
     let {data} = props;
     useEffect(() => {
         data &&
         data.map(image => {
             image["comments"] = [];
+            return image;
+        });
+    }, [data]);
+    useEffect(() => {
+        data &&
+        data.map(image => {
+            image["liked"] = false;
             return image;
         });
     }, [data]);
@@ -66,8 +75,13 @@ export default function ImageCard(props) {
 
         setComment("");
     };
+    const onLikeImage = index => {
+        console.log("inside", index);
+        let l = data[index].liked;
+        data[index].liked = !l;
+        setRender(!render);
+    };
 
-    console.log("data...", data);
   return (
       <Grid
           container
@@ -77,77 +91,89 @@ export default function ImageCard(props) {
           alignItems="center"
       >
           {props.data &&
-          props.data.map((person, index) => (
-          <Grid item xs={6} key={person.id}>
-              <Card className={classes.card} variant="outlined">
-                  <CardHeader
-                      avatar={
-                          <Avatar
-                              alt={person.user.full_name}
-                              src={person.user.profile_picture}
-                          />
-                      }
-                      title={person.user.username}
-                      subheader={changeToDate(person.caption.created_time)}
-                  />
-                  <CardMedia
-                      className={classes.media}
-                      image={person.images.standard_resolution.url}
-                      title="Paella dish"
-                  />
-                  <CardContent>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                          {person.caption.text.split("#")[0]}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                          {person.tags.map(tag => {
-                              return (
-                                  <Button key={tag} size="small" color="primary">
-                                      #{tag}
-                                  </Button>
-                              );
-                          })}
-                      </Typography>
-                      <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          component="ul"
-                      >
-                          {person.comments &&
-                          person.comments.length > 0 &&
-                          person.comments.map(tag => {
-                              return (
+          props.data.map((imageObj, index) => (
+              <Grid item xs={6} key={imageObj.id}>
+                  <Card className={classes.card} variant="outlined">
+                      <CardHeader
+                          avatar={
+                              <Avatar
+                                  alt={imageObj.user.full_name}
+                                  src={imageObj.user.profile_picture}
+                              />
+                          }
+                          title={imageObj.user.username}
+                          subheader={changeToDate(imageObj.caption.created_time)}
+                      />
+                      <CardMedia
+                          className={classes.media}
+                          image={imageObj.images.standard_resolution.url}
+                          title="Paella dish"
+                      />
+                      <CardContent>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                              {imageObj.caption.text.split("#")[0]}
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                              {imageObj.tags.map(tag => {
+                                  return (
+                                      <Button key={tag} size="small" color="primary">
+                                          #{tag}
+                                      </Button>
+                                  );
+                              })}
+                          </Typography>
+                          <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="ul"
+                          >
+                              {imageObj.comments &&
+                              imageObj.comments.length > 0 &&
+                              imageObj.comments.map(tag => {
+                                  return (
                         <li size="small" key={tag} color="primary">
                             {tag}
                         </li>
-                              );
-                          })}
-                      </Typography>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                      <IconButton aria-label="add to favorites">
-                          <FavoriteIcon/>
-                      </IconButton>
-                      <span>{person.likes.count} likes</span>
-                  </CardActions>
-                  <div style={{margin: "1rem"}}>
-                      <form className={classes.root} noValidate autoComplete="off">
-                          <TextField
-                              id={"add-comment" + index}
-                              label="Add a comment"
-                              onChange={commentChangeHandler}
-                          />
-                          <Button
-                              variant="contained"
-                              id="add-comments-button"
-                              color="primary"
-                              onClick={() => updateComment(index)}
+                                  );
+                              })}
+                          </Typography>
+                      </CardContent>
+                      <CardActions disableSpacing>
+                          <IconButton
+                              aria-label="add to favorites"
+                              onClick={() => onLikeImage(index)}
                           >
-                              Add
-                          </Button>
-                      </form>
-                  </div>
-              </Card>
+                              {data[index].liked ? (
+                                  <FavoriteIcon style={{color: red[500]}}/>
+                              ) : (
+                                  <FavoriteBorderIcon/>
+                              )}
+                          </IconButton>
+                          <span>
+                  {data[index].liked
+                      ? imageObj.likes.count + 1
+                      : imageObj.likes.count}{" "}
+                              likes
+                </span>
+                      </CardActions>
+                      <div style={{margin: "1rem"}}>
+                          <form className={classes.root} noValidate autoComplete="off">
+                              <TextField
+                                  id={"add-comment" + index}
+                                  label="Add a comment"
+                                  onChange={commentChangeHandler}
+                              />
+                              <Button
+                                  variant="contained"
+                                  id="add-comments-button"
+                                  color="primary"
+                                  onClick={() => updateComment(index)}
+                              >
+                                  Add
+                              </Button>
+                          </form>
+                      </div>
+                  </Card>
           </Grid>
           ))}
       </Grid>
